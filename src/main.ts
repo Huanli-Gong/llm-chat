e4949bbd8b245b618eb8e7a410f1325a8f45dfaf
Huanli Gong <hg163@duke.edu>
2024-05-27 10:52:22 +0800

    initial

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
+
+createApp(App)
+	.use(BootstrapVue)
+	.use(BootstrapVueIcons)
+	.mount('#app')
